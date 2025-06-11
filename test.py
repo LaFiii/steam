c8bd3e63ceffbe6cc69b61b59dfa2af79868dd50
Виктор Воробьёв <victor.vorobiev17@gmail.com>
2025-06-11 22:19:06 +0300

    Поменял чуть

--- test.py
+++ test.py
@@ -1,37 +1,34 @@
 from selenium import webdriver
-import time
-import random
 from faker import Faker
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException,NoSuchElementException
 
 fake = Faker()
-
-
-def data_generator():
-    username = fake.user_name()
-    password = fake.password(
-        length=random.randint(2, 50)
-    )
-    return username, password
-
-
-login_str, pwd_str = data_generator()
+login_str = fake.user_name()
+pwd_str = fake.password()
 driver = webdriver.Chrome()
-driver.get("https://store.steampowered.com/")
-time.sleep(5)
-login_btn = driver.find_element("xpath", "//*[contains(@class,'global_action_link') and contains(@href,'/login')]")
+wait = WebDriverWait(driver, 10)
+url = "https://store.steampowered.com/"
+
+driver.get(url)
+login_btn = wait.until(EC.element_to_be_clickable((By.XPATH,
+        "//*[contains(@class,'global_action_link') and contains(@href,'/login')]")))
 login_btn.click()
-time.sleep(10)
-login = driver.find_element("xpath", "//*[@type='text']")
+login = wait.until(EC.visibility_of_element_located((By.XPATH, "//input[@type='text']")))
 login.send_keys(login_str)
-password = driver.find_element("xpath", "//*[@type='password']")
+password = wait.until(EC.visibility_of_element_located((By.XPATH, "//input[@type='password']")))
 password.send_keys(pwd_str)
-time.sleep(5)
-login_btn = driver.find_element("xpath", "//*[@type='submit' and contains(text(), 'Войти')]")
+login_btn = wait.until(EC.element_to_be_clickable((By.XPATH,
+        "//button[@type='submit' and contains(normalize-space(.), 'Войти')]")))
 login_btn.click()
-time.sleep(10)
-error = driver.find_element("xpath",
-                            "//*[contains(text(),'Пожалуйста, проверьте свой пароль и имя аккаунта и попробуйте снова.')]")
-if error:
+try:
+    short_wait = WebDriverWait(driver,5)
+    short_wait.until(EC.visibility_of_element_located((By.XPATH,
+        "//*[contains(text(),'Пожалуйста, проверьте свой пароль и имя аккаунта и попробуйте снова.')]")))
     print("Ошибка входа: неверный логин/пароль")
-else:
+except (TimeoutException,NoSuchElementException):
     print("Сообщение об ошибке не найдено")
+finally:
+    driver.quit()
