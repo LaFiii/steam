eda80b96bb816a7871f24889660c0b0d6f8ddf22
Виктор Воробьёв <victor.vorobiev17@gmail.com>
2025-06-12 23:11:06 +0300

    Отредачил

--- test.py
+++ test.py
@@ -11,25 +11,22 @@ PASSWORD_FIELD = (By.XPATH, "//input[@type='password']")
 SUBMIT_BTN = (By.XPATH, "//button[@type='submit' and contains(normalize-space(.), 'Войти')]")
 ERROR_MESSAGE = (By.XPATH,
                  "//*[contains(text(),'Пожалуйста, проверьте свой пароль и имя аккаунта и попробуйте снова.')]")
+UNIQUE_LOCATOR = (By.XPATH,'//*[contains(@class, "btn_green_white_innerfade")]')
 
-
-@pytest.mark.parametrize("run_id", range(5))
-def test(driver, run_id):
+def test_auth(driver):
     fake = Faker()
+    time = 10
     login_str = fake.user_name()
     pwd_str = fake.password()
-    wait = WebDriverWait(driver, 10)
+    wait = WebDriverWait(driver, time)
     url = "https://store.steampowered.com/"
 
     driver.get(url)
+    wait.until(EC.visibility_of_element_located(UNIQUE_LOCATOR))
     wait.until(EC.element_to_be_clickable(LOGIN_PAGE_BTN)).click()
     wait.until(EC.visibility_of_element_located(USERNAME_FIELD)).send_keys(login_str)
     wait.until(EC.visibility_of_element_located(PASSWORD_FIELD)).send_keys(pwd_str)
     wait.until(EC.element_to_be_clickable(SUBMIT_BTN)).click()
 
-    try:
-        error = wait.until(EC.visibility_of_element_located(ERROR_MESSAGE))
-    except TimeoutException:
-        pytest.fail("Ожидали сообщение об ошибке логина, но его не было")
-    else:
-        assert "Пожалуйста, проверьте" in error.text
+    error = wait.until(EC.visibility_of_element_located(ERROR_MESSAGE))
+    assert "Пожалуйста, проверьте" in error.text
