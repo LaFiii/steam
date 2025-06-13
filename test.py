8e2ab7fbb35fa8423208e21ae18aa5ff85534b99
Виктор Воробьёв <victor.vorobiev17@gmail.com>
2025-06-13 03:38:08 +0300

    Ура йоу

--- test.py
+++ test.py
@@ -3,7 +3,6 @@ from faker import Faker
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-import time
 
 LOGIN_PAGE_BTN = (By.XPATH, "//*[contains(@class,'global_action_link') and contains(@href,'/login')]")
 USERNAME_FIELD = (By.XPATH, "//input[@type='text']")
@@ -33,6 +32,7 @@ def test_auth(login_page):
     wait.until(EC.visibility_of_element_located(USERNAME_FIELD)).send_keys(login_str)
     wait.until(EC.visibility_of_element_located(PASSWORD_FIELD)).send_keys(pwd_str)
     wait.until(EC.element_to_be_clickable(SUBMIT_BTN)).click()
-    time.sleep(1)
+    wait.until(EC.element_to_be_clickable(SUBMIT_BTN))
     error = wait.until(EC.visibility_of_element_located(ERROR_MESSAGE))
+
     assert "Пожалуйста, проверьте" in error.text
